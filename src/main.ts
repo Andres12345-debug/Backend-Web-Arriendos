c3b25023341f79950679a66b33f47dfbc3979918
Andres527 <julian2000mk1@gamil.com>
2025-03-12 21:17:38 -0500

    Cambios para el despliegue, en Main.ts y raiz del proyecto se creo vercel.json

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,30 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
+//despliegue import
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
 async function bootstrap() {
   const puerto = Number(process.env.PUERTO_SERVIDOR);
-  
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  
-  // Configurar servir archivos estáticos desde la carpeta uploads
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads/'
-  });
 
-  await app.listen(puerto, () => {
-    console.log(`Servidor funcionando puerto: ${puerto}`);
-  });
+  // Configurar servir archivos estáticos desde la carpeta uploads, MAQUINA LOCAL
+  //app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+  // prefix: '/uploads/'
+  //});
+
+  // Configurar archivos estáticos desde la carpeta uploads DESPLIEGUE
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
+  //en local
+  //await app.listen(puerto, () => {
+  // console.log(`Servidor funcionando puerto: ${puerto}`);
+  // });
+
+  //despliegue
+  await app.init();
 }
 bootstrap();
