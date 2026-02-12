3c191cf7e0a5aa11207033ff7b1447ff6a92b517
Andres12345-debug <julian2000mk1@gmail.com>
2026-02-11 22:17:56 -0500

    Cambios en puerto

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,20 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
-//despliegue import
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
 async function bootstrap() {
-  const puerto = Number(process.env.PUERTO_SERVIDOR);
+  const puerto =
+    Number(process.env.PORT) ||
+    Number(process.env.PUERTO_SERVIDOR) ||
+    3000;
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
 
-  // Configurar servir archivos estáticos desde la carpeta uploads, MAQUINA LOCAL
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-   prefix: '/uploads/'
+    prefix: '/uploads/',
   });
 
-  // Configurar archivos estáticos desde la carpeta uploads DESPLIEGUE
-  //app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
-
-  //en local
-  await app.listen(puerto, () => {
-   console.log(`Servidor funcionando puerto: ${puerto}`);
-   });
-
-  //despliegue
- // await app.init();
+  await app.listen(puerto);
+  console.log(`Servidor funcionando en puerto: ${puerto}`);
 }
 bootstrap();
