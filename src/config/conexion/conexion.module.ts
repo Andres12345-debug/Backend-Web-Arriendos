35a8a17a794cca51c99799c530918fb26d67b8da
Andres12345-debug <julian2000mk1@gmail.com>
2026-02-11 22:35:21 -0500

    Cambios en conexion

--- src/config/conexion/conexion.module.ts
+++ src/config/conexion/conexion.module.ts
@@ -19,7 +19,7 @@ import { SnakeNamingStrategy } from "typeorm-naming-strategies";
           const poolConexion = new DataSource({
             type: 'postgres',
             host: String(process.env.DB_HOST),
-            port: Number(process.env.PORT),
+              port: Number(process.env.DB_PORT), // ✅ AQUÍ estaba el error
             username: String(process.env.DB_USER),
             password: String(process.env.DB_PASSWORD),
             database: String(process.env.DB_NAME),
